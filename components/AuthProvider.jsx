2a0c213d0805133f1e1cdac217cba6b10c44de2d
BobtheBuilder-ux <client@bobbieberry.com>
2025-07-15 23:48:50 +0100

    Remove Supabase configuration and components

--- components/AuthProvider.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from 'react';
-import { AuthProvider as AuthProviderJS } from '../hooks/useAuth.js';
-
-export function AuthProvider({ children }) {
-  return <AuthProviderJS>{children}</AuthProviderJS>;
-}
